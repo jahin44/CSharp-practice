c08df9a5b05160a2e4cbfc17a8da5896a695a84f
jahin44 <jahinhasan1@gmail.com>
2020-12-30 20:19:32 +0600

    cs and csproj file add

---
+++ CSharpsolution/CSharp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YOUR TEXT");
+            string line = Console.ReadLine();
+            string[] linepart = line.Split(' ',',') ;//split as , and " " 
+            foreach(string a in linepart)
+            {
+                Console.WriteLine(a);
+            }
+
+        }
+    }
+}
