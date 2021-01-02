9f01dd9f159dfc4e6a5ff20b6528ea577f7d5e33
jahin44 <jahinhasan1@gmail.com>
2021-01-02 15:59:31 +0600

    array and randon int test

---
+++ CSharpsolution/data type and basic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace data_type_and_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random() ;
+            int[,] arr = new int[10,10];
+            for(int i = 0; i < 10; i++)
+            {  for(int j = 0; j <10; j++)
+                {
+                    arr[i,j] = ran.Next(1,100);
+                } 
+
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write( ran.Next(1,11)+"\t");
+
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+        }
+    }
+}
