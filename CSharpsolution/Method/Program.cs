3c51a2a65746d7d652db918c6a6551d50e5008c5
jahin44 <jahinhasan1@gmail.com>
2021-01-08 23:51:10 +0600

    split

--- CSharpsolution/Method/Program.cs
+++ CSharpsolution/Method/Program.cs
@@ -27,15 +27,15 @@ namespace CalculatorApplication
 
             static void Main(string[] args)
             {
-                /* local variable definition */
+                /*    local variable definition */
                 int a = 100;
-                int b = 200;
+                int b =  200;
                 int ret;
                 NumberManipulator n = new NumberManipulator();
 
                 //calling the FindMax method
                 ret = n.FindMax(a, b);
-                Console.WriteLine("Max value is : {0}", ret);
+                Console.WriteLine("Max value is : {0}",ret);
                 Console.ReadLine();
             }
         }
