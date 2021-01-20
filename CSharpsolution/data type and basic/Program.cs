42b6d8be9598807ed54abcd3b30cb6b8f249b030
jahin44 <jahinhasan1@gmail.com>
2021-01-20 21:18:53 +0600

    jahin

--- CSharpsolution/data type and basic/Program.cs
+++ CSharpsolution/data type and basic/Program.cs
@@ -11,11 +11,11 @@ namespace data_type_and_basic
             for(int i = 0; i < 10; i++)
             {  for(int j = 0; j <10; j++)
                 {
-                    arr[i,j] = ran.Next(1,100);
+                    arr[i,j] = ran. Next(1,100);
                 } 
 
             }
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10;i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
