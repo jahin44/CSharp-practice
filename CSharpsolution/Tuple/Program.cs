7f0254895b2eed2fb2aeba212d3b10389c5f1487
jahin44 <jahinhasan1@gmail.com>
2021-01-24 12:32:03 +0600

    Assignment_2 test commit

---
+++ CSharpsolution/Tuple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = DateTime.Now;
+            var name = "jahin";
+            var person = (fullName: name, 2, married: true, 35.4, time);
+            Console.WriteLine($"Name= {person.fullName}");
+            Console.WriteLine($"Age = {person.Item2}");
+            Console.WriteLine($"Married= {person.married}");
+            Console.WriteLine($"Hight= {person.Item4}");
+            Console.WriteLine($"Time= {person.Item5}");
+
+
+
+
+
+        }
+    }
+}
